4d0b4add612f82718e13ce7097ca68c7de5a05be
Amurallado <andaluh.crucero@gmail.com>
2025-12-08 23:14:54 +0100

    update: script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+// Todas las secciones tienen un diseño aleatorio
+const sections = document.querySelectorAll(".darkmode");
+function desing() {
+  const styleRandom = () => {
+    return Math.floor(Math.random() * 2);
+  };
+  sections.forEach((section) => {
+    if (styleRandom() === 1) {
+      section.classList.toggle("darkmode");
+    }
+  });
+}
+
+// Cambia la foto del persona y su trabajo
+const playeroptions = document.querySelectorAll(".hero__option");
+playeroptions.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    document.querySelector(".hero__title").textContent = e.target.alt;
+    document.querySelector(".hero__mypicture").src = e.target.src;
+  });
+});
+
+// FAQ Acordeones
+const questionsAcordeon = document.querySelectorAll(".questions__acordeon");
+questionsAcordeon.forEach((questionGroup) => {
+  questionGroup.addEventListener("click", () => {
+    const questionsAnswer = questionGroup.children[1];
+    questionsAnswer.classList.toggle("questions--hidden");
+  });
+});
+
+// Desbloquear elementos bloqueados
+const lockedSections = document.querySelectorAll(".locked");
+lockedSections.forEach((lockedElement) => {
+  let temporizador;
+  lockedElement.addEventListener("mousedown", () => {
+    temporizador = setTimeout(() => {
+      lockedElement.classList.remove("locked");
+    }, 2000); // 2 segundos
+  });
+
+  lockedElement.addEventListener("mouseup", () => {
+    clearTimeout(temporizador); // Cancela si se suelta antes
+  });
+
+  lockedElement.addEventListener("mouseleave", () => {
+    clearTimeout(temporizador); // Cancela si se sale del área
+  });
+});
+
+// Random Design Botones
+document.querySelector(".random__button").addEventListener("click", () => {
+  desing();
+});
+
+// Burger Menu
+document.querySelector(".header__burger").addEventListener("click", () => {
+  document.querySelector(".header__menu").classList.toggle("collapsed");
+});
+
+desing();
